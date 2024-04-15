86d386ad7d628f72972572c56eda7974808e4c6c
jackdow-ek <omargaribov1@gmail.com>
2024-04-15 03:03:41 +0300

    final

--- components/Header.tsx
+++ components/Header.tsx
@@ -96,32 +96,37 @@ const Header: React.FC<HeaderProps> = ({
                     </button>
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
-                    <button
-                        className="
-                        rounded-full
-                        p-2
-                        bg-white
-                        flex
-                        items-center
-                        justify-center
-                        hover:opacity-75
-                        transition"
-                    >
-                        <HiHome className="text-black" size={20} />
-                    </button>
-                    <button
-                        className="
-                        rounded-full
-                        p-2
-                        bg-white
-                        flex
-                        items-center
-                        justify-center
-                        hover:opacity-75
-                        transition"
-                    >
-                        <BiSearch className="text-black" size={20} />
-                    </button>
+                    <a href="/">
+                        <button
+                            className="
+                            rounded-full
+                            p-2
+                            bg-white
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition"
+                        >
+                            <HiHome className="text-black" size={20} />
+                        </button>
+                    </a>
+                    <a href="/search">
+                        <button
+                            className="
+                            rounded-full
+                            p-2
+                            bg-white
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition"
+                            
+                        >
+                            <BiSearch className="text-black" size={20} />
+                        </button>
+                    </a>
                 </div>
                 <div 
                     className="
